43f9db7b34458b2a609a90627ddb576b7ee2eaf0
AliHosseini370 <smahosseini370@gmail.com>
2023-11-06 09:59:30 +0330

    second

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://alihosseini370.github.io/simple-chat-app',
   plugins: [react()],
 })
